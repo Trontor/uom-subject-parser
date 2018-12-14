3979f1e60650a26438cb7bb4df0e9fa75d0c8d3a
Trontor <Trontor@users.noreply.github.com>
2018-12-14 11:01:48 +1100

    added request headers for safety + reading in current files if exists

--- subject-scraper.js
+++ subject-scraper.js
@@ -20,7 +20,10 @@ class SubjectInfo {
 const generateSearchURL = (year, studyPeriod) => `https://handbook.unimelb.edu.au/search?query=&year=${year}&types%5B%5D=subject&study_periods%5B%5D=${studyPeriod}`;
 
 const getHTML = (URL, callback) => {
-  request(URL, function(error, response, body) {
+  var customHeaderRequest = request.defaults({
+    'headers': {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
+  });
+  customHeaderRequest.get(URL, function(error, response, body) {
     callback(body);
   });
 };
@@ -34,16 +37,23 @@ const getPageCount = (baseURL, callback) => {
   });
 };
 
-const scrapePage = (baseURL, number, maxPages, callback, finishedCallBack) => {
+const scrapePage = (year, studyPeriod, baseURL, number, maxPages, callback, finishedCallBack) => {
   const pageURL = baseURL + `&page=${number}`;
   getHTML(pageURL, html => {
     const pageSubjects = [];
     const $ = cheerio.load(html);
     const list = $(".search-results__accordion > li");
+    if (list.length !== 20){
+      //console.log(html);
+    }
     list.each(function() {
-      const subjectCode = $(this).find(".search-results__accordion-code").text();
-      const title = $(this).find(".search-results__accordion-title").text().replace(subjectCode, "");
-      const details = $(this).find(".search-results__accordion-detail").text();
+      const subjectCode = $(this).find(".search-results__accordion-code").text().trim();   
+      if (allSubjects.filter(val => val.code === subjectCode).length != 0) {
+        //console.log(`\t|-> ${subjectCode} already exists, skipping!`);
+        return;
+      }
+      const title = $(this).find(".search-results__accordion-title").text().replace(subjectCode, "").trim();
+      const details = $(this).find(".search-results__accordion-detail").text().trim();
       const leftBound = "Offered:";
       const rightBound = "Year:";
       const offered = details.substring(
@@ -53,7 +63,7 @@ const scrapePage = (baseURL, number, maxPages, callback, finishedCallBack) => {
       let studyPeriods = offered.split(",").map(item => item.trim());
       const newSubject = new SubjectInfo(subjectCode, title, studyPeriods);
       pageSubjects.push(newSubject);
-    });
+    }); 
     pagesScraped++;
     callback(pageSubjects);
     if (pagesScraped == maxPages) {
@@ -63,31 +73,55 @@ const scrapePage = (baseURL, number, maxPages, callback, finishedCallBack) => {
 };
 
 let pagesScraped = 0;
+let allSubjects = [];
 const scrapeSubjects = (year, studyPeriod, finishedCallBack) => {
   pagesScraped = 0;
-  let allSubjects = [];
+  allSubjects = [];
+  const currentFileName = `subjects_${year}_${studyPeriod}.json`;
+  try {
+    allSubjects = JSON.parse(fs.readFileSync(currentFileName, 'utf8'));
+    console.log(`Read existing subject file ${allSubjects.length} entires existing.`); 
+  } catch (error){
+    // Probably no file existing.
+  }
   const baseURL = generateSearchURL(year, studyPeriod);
   getPageCount(baseURL, count => {
     console.log(`There are ${count} pages to parse from ${decodeURI(baseURL)}.\n`);
     const scrapeBar = new _cliProgress.Bar({}, progressBarStyle);
-    scrapeBar.start(count, 1);
-    for (let pageNo = 1; pageNo <= count; pageNo++) {
-      scrapePage(
-        baseURL,
-        pageNo,
-        count,
-        pageSubjects => {
-          allSubjects = allSubjects.concat(pageSubjects);
-          scrapeBar.update(pagesScraped);
-        },
-        () => {
-            scrapeBar.stop();
-            finishedCallBack(allSubjects);
-        }
-      );
-    }
+    //scrapeBar.start(count, 1);
+    invokePageScraper(year, studyPeriod, baseURL, finishedCallBack, count);
   });
 };
+
+const invokePageScraper = (year, studyPeriod, baseURL, finishedCallBack, maxCount, pageNum = 1)  => {
+  console.log(`|-> Page ${pageNum}/${maxCount} : ${baseURL}&page=${pageNum}`);
+  scrapePage(
+    year, 
+    studyPeriod,
+    baseURL,
+    pageNum,
+    maxCount,
+    pageSubjects => {
+      allSubjects = allSubjects.concat(pageSubjects);
+      if (pageSubjects.length > 0){
+        process.stdout.write(`\t|-> `);
+        pageSubjects.forEach(subject=>{
+          process.stdout.write(`${subject.code}, `);
+        });
+        console.log();  
+        console.log(`\t|-> Parsed ${pageSubjects.length} subjects.`); 
+      }    
+      if (pageNum != maxCount){
+        invokePageScraper(year, studyPeriod, baseURL, finishedCallBack, maxCount, ++pageNum);
+      }
+      //scrapeBar.update(pagesScraped);
+    },
+    () => {
+        //scrapeBar.stop();
+        finishedCallBack(allSubjects);
+    }
+  );
+}
 
 const studyPeriods = [
     {
@@ -107,9 +141,27 @@ const studyPeriods = [
         "name": "Winter Term"
     }
 
-    /* Year long and others not handled */
+    /* year long and others not handled */
 ];
  
+
+const duplicateAnalysis = allSubjects =>{
+    let duplicates = 0;
+    allSubjects.forEach((val, i)=>{
+      allSubjects.forEach((comp, j)=>{
+        if (i === j || val.code !== comp.code){
+          return;
+        } 
+        //console.log(val.code + " has duplicates.");
+        duplicates++;
+      });
+    });
+    const total =  allSubjects.length;
+    const perc = duplicates/total *100;
+    if (perc > 0){
+      console.log(`${perc.toFixed(2)}% duplicates exist.`);
+    }
+}
 const scrapeYear = (year, finishedScrapingCallback, studyPeriodIndex = 0) => {
     const studyPeriodName = studyPeriods[studyPeriodIndex].name;
     console.log(`Scraping subjects for study period: ${studyPeriodName} in ${year}`);
@@ -120,12 +172,14 @@ const scrapeYear = (year, finishedScrapingCallback, studyPeriodIndex = 0) => {
         const timeSpent = (end - begin) / 1000;
         console.log(`\nA total of ${allSubjects.length} subjects were parsed in ${timeSpent} seconds.`);
         const fileContents = JSON.stringify(allSubjects);
-        const outputFileName = `subjects_${year}_${semCode}.json`;
+        const outputFileName = `subjects_${year}_${semCode}.json`;    
+        duplicateAnalysis(allSubjects);
         fs.writeFile(outputFileName, fileContents, error => {
           if (error) {
             return console.log(`Could not save file, error encountered!\n${error}`);
           }
-          console.log(`Subject information was saved in JSON format to ${outputFileName}!\n`)
+          console.log(`Subject information was saved in JSON format to ./${outputFileName}!\n`);
+          console.log('--------------------------------------------------------------------');
           if (studyPeriodIndex < studyPeriods.length - 1){
             scrapeYear(year, finishedScrapingCallback, ++studyPeriodIndex);
           } else {
@@ -144,9 +198,9 @@ const startYear = thisYear - 1;
 let currentYear = startYear;
 const finishedScrapingYear = ()=>{
     if (currentYear < nextYear){
-        scrapeYear(++currentYear, finishedScrapingYear);
+      //scrapeYear(++currentYear, finishedScrapingYear);
     }
 };
 
 /* driver function */
-scrapeYear(startYear, finishedScrapingYear);
+scrapeYear(2019, finishedScrapingYear);
